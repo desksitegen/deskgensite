1b9fdc94a3aa4521eea5b424c55ca7b5c7a363e3
desksitegen <desksiteevendas@gmail.com>
2026-03-28 00:45:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let keyCorreta = [
+"DESK-EIGL",
+"DESK-XDQX",
+"DESK-JSGX"
+]
+let contador = 0
+
+function login(){
+
+let key = document.getElementById("key").value
+
+if(keyCorreta.includes(key)){
+
+document.getElementById("login").style.display="none"
+document.getElementById("painel").style.display="block"
+
+}
+
+else{
+
+document.getElementById("erro").innerText="KEY inválida"
+
+}
+
+}
+
+function gerar(){
+
+let tipo = document.getElementById("tipo").value
+let resultado = document.getElementById("resultado")
+
+let nome = Math.random().toString(36).substring(2,8)
+let numero = Math.floor(Math.random()*9999)
+
+let senha = Math.random().toString(36).substring(2,10)
+
+let conta
+
+if(tipo=="gmail"){
+
+conta = nome+numero+"@gmail.com | "+senha
+
+}
+
+if(tipo=="outlook"){
+
+conta = nome+numero+"@outlook.com | "+senha
+
+}
+
+if(tipo=="netflix"){
+
+conta = "user"+numero+"@email.com | "+senha
+
+}
+
+if(tipo=="spotify"){
+
+conta = "spotify"+numero+"@email.com | "+senha
+
+}
+
+resultado.value = conta
+
+contador++
+
+document.getElementById("count").innerText = contador
+
+}
